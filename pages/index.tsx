9f16dbf78383e06b713af01b752c112c8f9dc829
CRamos132 <camille.gpramos@gmail.com>
2021-09-24 11:05:20 -0300

    create base for design

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,22 @@ const Home: NextPage = () => {
   return (
     <Box width="100%">
       <Flex width="100%" backgroundColor="#6324C6" height="91px" direction="row" justifyContent="center" alignItems="center">REACTJS</Flex>
-      <Button onClick={busca}>Busca</Button>
+      <Flex direction="row" alignItems="center" justifyContent="space-evenly" width="60%" marginY="21px" marginX="auto">
+        <Button onClick={busca}>Busca</Button>
+        <Button onClick={busca}>Busca</Button>
+        <Button onClick={busca}>Busca</Button>
+      </Flex>
+      <Flex width="90%" direction="column" margin="auto">
+        <Flex direction="row" borderTop="1px solid gray" paddingY="12px">
+          <Box borderRadius="16px" height="77px" width="77px" backgroundColor="gray.400" />
+          <Flex direction="column" justifyContent="space-between" marginLeft="13px">
+            <Box>Titulo</Box>
+            <Box>Data</Box>
+            <Box>Dominio</Box>
+          </Flex>
+        </Flex>
+        <Button onClick={busca}>Busca</Button>
+      </Flex>
     </Box>
   );
 };
